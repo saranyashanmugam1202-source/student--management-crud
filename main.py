d8800b1c4e449845639a3c3c96a66f6e5dff1906
saranyashanmugam1202-source <saranyashanmugam1202@gmail.com>
2026-04-10 23:47:43 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+import mysql.connector
+
+def connect_db():
+    # Add your MySQL connection code here
+    pass
+
+def add_student():
+    # CRUD: Create operation
+    pass
+
+def view_students():
+    # CRUD: Read operation  
+    pass
+
+if __name__ == "__main__":
+    print("Student Management System")
